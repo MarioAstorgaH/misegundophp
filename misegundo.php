7e59613dae9bc9caa87075abd40107b86e5e7dae
Mario <astorgamhdez@gmail.com>
2025-02-06 09:59:21 -0700

    1k con foreach

--- misegundo.php
+++ misegundo.php
@@ -1,13 +1,6 @@
 <h1>Practica</h1>
 <br>
 <?php
-$i=1;
-for($i;$i<=1000;$i++){
-    if($i%10==0){
-        echo "$i.<br>";
-    }
-    else{
-        echo "$i, ";
-    }
-}
+foreach(range(1,1000) as $i)
+    ($i%10==0)?print "$i<br>":print "$i, ";
 ?>
